b61a0a1178b26633788dcb18c7409c4c4e4e615c
Ethan_Hadley <ekhadley@gmail.com>
2023-08-10 11:55:54 -0500

    tweaking model, tracing forward pass

--- agent.py
+++ agent.py
@@ -8,22 +8,25 @@ from utils import *
 
 
 class policy(nn.Module):
-    def __init__(self, boardShape, lr=.001, stationary=True, cuda=False):
+    def __init__(self, boardShape, lr=.001, stationary=True, cuda=False, leaky=True):
         super(policy, self).__init__()
         self.boardShape, self.lr = boardShape, lr
+        self.leaky = leaky
         self.cuda_ = cuda
         h, w = boardShape
 
-        self.conv1 = nn.Conv2d(in_channels=2, out_channels=128, kernel_size=3, padding=1)
-        self.act1 = nn.LeakyReLU()
-        
-        self.conv2 = nn.Conv2d(in_channels=128, out_channels=64, kernel_size=3, padding=1)
-        self.act2 = nn.LeakyReLU()
-        
-        self.conv3 = nn.Conv2d(in_channels=64, out_channels=32, kernel_size=3, padding=1)
-        self.act3 = nn.LeakyReLU()
-        
-        self.lin1 = nn.Linear(32*h*w, 512)
+        self.conv1 = nn.Conv2d(in_channels=2, out_channels=32, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, padding=1)
+        self.conv3 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, padding=1)
+        if leaky:
+            self.act1 = nn.LeakyReLU()
+            self.act2 = nn.LeakyReLU()
+            self.act3 = nn.LeakyReLU()
+        else:
+            self.act1 = nn.ReLU()
+            self.act2 = nn.ReLU()
+            self.act3 = nn.ReLU()
+        self.lin1 = nn.Linear(64*h*w, 512)
         self.act4 = nn.ReLU()
         
         self.lin2 = nn.Linear(512, 256)
@@ -37,10 +40,10 @@ class policy(nn.Module):
         
         if self.cuda_: self.to("cuda")
 
-        #self.opt = torch.optim.AdamW(self.parameters(), lr=lr, betas=(0.999, 0.999) if stationary else None)
-        self.opt = torch.optim.SGD(self.parameters(), lr=lr, weight_decay=1e-4)
+        self.opt = torch.optim.AdamW(self.parameters(), lr=lr, weight_decay=.001, betas=(0.99, 0.99) if stationary else None)
+        #self.opt = torch.optim.SGD(self.parameters(), lr=lr, weight_decay=.0003)
 
-    def forward(self, X:torch.tensor, allowed:torch.tensor):
+    def forward(self, X:torch.Tensor, allowed:torch.Tensor):
         if self.cuda_: X, allowed = X.to("cuda"), allowed.to("cuda")
         X = self.act1(self.conv1(X))
         X = self.act2(self.conv2(X))
@@ -53,7 +56,7 @@ class policy(nn.Module):
         X += (1e12*(allowed-1)) # puts the pre-softmax values of illegal moves to -inf
         X = self.act7(X)
         return X
-    def __call__(self, X:torch.tensor, allowed:torch.tensor): return self.forward(X, allowed)
+    def __call__(self, X:torch.Tensor, allowed:torch.Tensor): return self.forward(X, allowed)
     
     def train(self, states, allowed, actions, weights):
         if self.cuda_:
@@ -87,10 +90,11 @@ class vpoAgent:
         self.boardSize, self.lr, self.color = boardSize, lr, color
         self.numActions = boardSize[1]
         self.cuda_ = cuda
+        
         self.policy = policy(boardSize, lr=lr, stationary=stationary, cuda=cuda)
-
-        #exst, exal = self.observe(newBoard(boardSize)), torch.tensor(np.ones(boardSize[1]))
-        #self.policy.forward = torch.jit.trace(exst, exal)
+        exst = torch.tensor(np.zeros(shape=(1, 2, *boardSize), dtype=np.float32))
+        exal = torch.tensor(np.ones(shape=(self.numActions), dtype=np.float32))
+        self.policy.forward = torch.jit.trace(self.policy.forward, example_inputs=(exst, exal))
 
         self.states, self.allowed, self.actions, self.weights = [], [], [], []
     
@@ -108,7 +112,7 @@ class vpoAgent:
         dist = self.policy(state, allowed)
         dist = torch.distributions.Categorical(dist)
         action = dist.sample()
-        return action
+        return dist, action
 
     def drop(self, board, col):
         return drop(board, col, self.color)
@@ -133,7 +137,7 @@ class vpoAgent:
     def load(self, path):
         sd = torch.load(path)
         self.policy.load_state_dict(sd)
-    def loadDict(self, sd):
+    def loadPolicy(self, sd):
         self.policy.load_state_dict(sd)
     def stateDict(self):
         return self.policy.state_dict()
